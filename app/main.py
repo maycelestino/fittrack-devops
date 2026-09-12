e115de892c30e36eb0a1d8b6bfeafb4ab1a4540d
Mayara Celestino <mayaracelestinoo@gmail.com>
2026-09-12 17:16:21 -0300

    feat: add exercise creations and linting endpoints

--- app/main.py
+++ app/main.py
@@ -6,7 +6,24 @@ app = FastAPI(
     version="1.0.0"
 )
 
+exercises = []
+
 
 @app.get("/")
 def home():
     return {"message": "FitTrack API funcionando!"}
+
+
+@app.get("/exercises")
+def list_exercises():
+    return exercises
+
+
+@app.post("/exercises")
+def create_exercise(exercise: dict):
+    exercises.append(exercise)
+
+    return {
+        "message": "Exercício cadastrado com sucesso!",
+        "exercise": exercise
+    }
